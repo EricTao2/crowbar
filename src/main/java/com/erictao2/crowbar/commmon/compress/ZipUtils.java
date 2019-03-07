80547a14b03f6f92ccb15823c08a37deeee3ad0d
TJX <823222209@qq.com>
2019-03-07 16:48:14 +0800

    Initial commit

---
+++ src/main/java/com/erictao2/crowbar/commmon/compress/ZipUtils.java
@@ -0,0 +1,130 @@
+package com.erictao2.crowbar.commmon.compress;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipUtils {
+    private String zipFileName;      // 目的地Zip文件
+    private String sourceFileName;   //源文件（待压缩的文件或文件夹）
+    private boolean isDeleteSource = false;
+
+
+    public ZipUtils(String zipFileName, String sourceFileName) {
+        this.zipFileName = zipFileName;
+        this.sourceFileName = sourceFileName;
+    }
+
+    public ZipUtils(String zipFileName, String sourceFileName, boolean isDeleteSource) {
+        this(zipFileName, sourceFileName);
+        this.isDeleteSource = isDeleteSource;
+    }
+
+    /***
+     * 删除指定文件夹下所有文件
+     *
+     * @param path 文件夹完整绝对路径
+     * @return
+     */
+    public static boolean delAllFile(String path) {
+        boolean flag = false;
+        File file = new File(path);
+        if (!file.exists()) {
+            return flag;
+        }
+        if (!file.isDirectory()) {
+            return flag;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                delAllFile(path + "/" + tempList[i]);// 先删除文件夹里面的文件
+                delFolder(path + "/" + tempList[i]);// 再删除空文件夹
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    /***
+     * 删除文件夹
+     * @param folderPath 文件夹完整绝对路径
+     */
+    public static void delFolder(String folderPath) {
+        try {
+            delAllFile(folderPath); // 删除完里面所有内容
+            String filePath = folderPath;
+            filePath = filePath.toString();
+            File myFilePath = new File(filePath);
+            myFilePath.delete(); // 删除空文件夹
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void zip() throws Exception {
+        //File zipFile = new File(zipFileName);
+
+        //创建zip输出流
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
+
+        //创建缓冲输出流
+        BufferedOutputStream bos = new BufferedOutputStream(out);
+
+        File sourceFile = new File(sourceFileName);
+
+        //调用函数
+        compress(out, bos, sourceFile, sourceFile.getName());
+
+        bos.close();
+        out.close();
+        if (isDeleteSource) {
+            delFolder(sourceFileName);
+        }
+
+    }
+
+    public void compress(ZipOutputStream out, BufferedOutputStream bos, File sourceFile, String base) throws Exception {
+        //如果路径为目录（文件夹）
+        if (sourceFile.isDirectory()) {
+
+            //取出文件夹中的文件（或子文件夹）
+            File[] flist = sourceFile.listFiles();
+
+            //如果文件夹为空，则只需在目的地zip文件中写入一个目录进入点
+            if (flist.length == 0) {
+                System.out.println(base + "/");
+                out.putNextEntry(new ZipEntry(base + "/"));
+                //如果文件夹不为空，则递归调用compress，文件夹中的每一个文件（或文件夹）进行压缩
+            } else {
+                for (int i = 0; i < flist.length; i++) {
+                    compress(out, bos, flist[i], base + "/" + flist[i].getName());
+                }
+            }
+            //如果不是目录（文件夹），即为文件，则先写入目录进入点，之后将文件写入zip文件中
+        } else {
+            out.putNextEntry(new ZipEntry(base));
+            FileInputStream fos = new FileInputStream(sourceFile);
+            BufferedInputStream bis = new BufferedInputStream(fos);
+
+            int tag;
+            System.out.println(base);
+            //将源文件写入到zip文件中
+            while ((tag = bis.read()) != -1) {
+                bos.write(tag);
+            }
+            bis.close();
+            fos.close();
+
+        }
+    }
+}
